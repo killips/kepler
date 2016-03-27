b3ac9deb913a65e01cd732688088bac1a6852522
killips <ivansmirnov63@gmail.com>
2016-03-27 18:56:23 +0300

    second commit

--- src/kepler/Main.java
+++ src/kepler/Main.java
@@ -5,11 +5,15 @@
  */
 package kepler;
 
+import java.io.IOException;
 import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -18,34 +22,23 @@ import javafx.stage.Stage;
  * @author Пользователь
  */
 public class Main extends Application {
-    
+    private  Stage primaryStage;
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader load = new FXMLLoader();
+        AnchorPane root = (AnchorPane) load.load(getClass().getResourceAsStream("GUI.fxml"));
+        primaryStage.setTitle("Kepler - город мечты, Транспортная сеть");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
-
+    
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         launch(args);
     }
-    
 }
